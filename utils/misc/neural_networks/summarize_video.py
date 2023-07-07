787d2a0eed4787a5529b13a09a29ec54bcc4feda
medlcago <sasha.vol40@mail.ru>
2023-07-07 17:37:19 +0300

    bug fixed and code readability improved

--- utils/misc/neural_networks/summarize_video.py
+++ utils/misc/neural_networks/summarize_video.py
@@ -2,50 +2,51 @@ import openai
 import re
 from openai import InvalidRequestError
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
-
+from typing import Optional
 from data import config
 
 
-def extract_youtube_video_id(url: str) -> str | None:
+def extract_youtube_video_id(url: str) -> Optional[str]:
     """
     Links example:
-        1. 'https://www.youtube.com/watch?v=video_id',
-        2. 'https://m.youtube.com/watch?v=video_id',
-        3. 'https://youtu.be/video_id',
-        4. 'https://www.youtube.com/embed/video_id',
+    1. 'https://www.youtube.com/watch?v=video_id',
+    2. 'https://m.youtube.com/watch?v=video_id',
+    3. 'https://youtu.be/video_id',
+    4. 'https://www.youtube.com/embed/video_id',
     """
-
     match = re.search(r'(?:youtube\.com/(?:watch\?v=|embed/)|youtu\.be/)([a-zA-Z0-9_-]+)', url)
     if match:
         video_id = match.group(1)
         return video_id
 
 
-def get_video_transcript(video_id: str) -> str | None:
-    """
-    Fetch the transcript of the provided YouTube video
-    """
+def get_video_transcript(video_id: str) -> Optional[str]:
     try:
         transcript = YouTubeTranscriptApi.get_transcript(video_id=video_id, languages=["ru", "en", "de", "es"])
     except TranscriptsDisabled:
-        # The video doesn't have a transcript
         return None
 
     text = " ".join([line["text"] for line in transcript])
     return text
 
 
-def generate_summary(text: str, language="ru") -> str:
-    """
-    Generate a summary of the provided text using OpenAI API
-    """
+def configure_openai_api():
     openai.api_key = config.OpenAI_API_KEY
 
-    # Use GPT to generate a summary
-    instructions = "Пожалуйста, резюмируйте предоставленный текст на русском языке" if language == "ru" else "Please, summarize the provided text in English"
+
+def generate_summary(text: str, language: str = "ru") -> str:
+    configure_openai_api()
+
+    instructions_map = {
+        "ru": "Пожалуйста, резюмируйте предоставленный текст на русском языке",
+        "en": "Please, summarize the provided text in English"
+    }
+
+    instructions = instructions_map.get(language, instructions_map["en"])
+
     try:
         response = openai.ChatCompletion.create(
-            model=config.model,
+            model=config.models["gpt-3"],
             messages=[
                 {"role": "system", "content": instructions},
                 {"role": "user", "content": text}
@@ -57,28 +58,22 @@ def generate_summary(text: str, language="ru") -> str:
             frequency_penalty=0.1,
         )
 
-        # Return the generated summary
         return response.choices[0].message.content.strip()
     except InvalidRequestError as error:
         print(error)
         return "К сожалению, превышена максимальная длина сообщения :(" if language == "ru" else "Unfortunately, the maximum message length has been exceeded :("
 
 
-def summarize_youtube_video(video_url: str, language="ru") -> str:
-    """
-    Summarize the provided YouTube video
-    """
-    # Extract the video ID from the URL
+def summarize_youtube_video(video_url: str, language: str = "ru") -> str:
     video_id = extract_youtube_video_id(video_url)
 
-    # Fetch the video transcript
     if not video_id:
-        return "The link is not a link to a youtube video"
+        return "Ссылка не является ссылкой на видео с YouTube"
+
     transcript = get_video_transcript(video_id)
 
-    # If no transcript is found, return an error message
     if not transcript:
-        return f"No transcript found for this video in English or Russian: {video_url}"
+        return f"Для данного видео нет транскрипта на английском или русском языке: {video_url}"
 
     summary = generate_summary(transcript, language=language)
 
