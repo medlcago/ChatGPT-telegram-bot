28efaf987c5101f2cbd3b5556425681ad0626c76
medlcago <sasha.vol40@mail.ru>
2023-08-16 16:25:17 +0300

    improving informational messages for the user

--- decorators/check_time_limits.py
+++ decorators/check_time_limits.py
@@ -3,6 +3,7 @@ from functools import wraps
 
 import pytz
 from aiogram import types
+from aiogram.utils.markdown import hbold
 
 from data import config
 from loader import db
@@ -25,12 +26,14 @@ class CheckTimeLimits:
         command_count = await db.get_command_count(user_id)
         last_command_time = await db.get_last_command_time(user_id)
 
+        wait_time = timedelta(seconds=30)
+
         if last_command_time is not None:
             time_since_last_command = now - last_command_time
 
-            if time_since_last_command < timedelta(seconds=30):
-                time_period = (timedelta(seconds=30) - time_since_last_command).seconds
-                await message.reply(f"⏳ Подождите еще {time_period} секунд перед тем, как отправить следующий запрос..")
+            if time_since_last_command < wait_time:
+                seconds = (wait_time - time_since_last_command).seconds
+                await message.reply(f"⏳ Подождите еще {hbold(self._get_seconds_suffix(seconds))} перед тем, как отправить следующий запрос..")
                 return
 
             if time_since_last_command > timedelta(hours=1):
@@ -42,8 +45,29 @@ class CheckTimeLimits:
             await db.update_last_command_time(user_id, now.strftime(date_format))
             return await self.handler(message, *args, **kwargs)
 
-        date_format = '%d.%m.%Y %H:%M:%S'
+        wait_time = timedelta(hours=1)
+        elapsed_time = datetime.now(moscow_tz) - last_command_time
+        remaining_time = (wait_time - elapsed_time)
+        minutes = remaining_time // timedelta(minutes=1)
+
         await message.reply(
-            f"Превышен часовой лимит запросов.\nПопробуйте снова <code>{(last_command_time + timedelta(hours=1)).strftime(date_format)}</code>"
+            f"Превышен часовой лимит запросов.\nПопробуйте снова через {hbold(self._get_minutes_suffix(minutes))}"
             f"\n\nПодробнее /limits")
         return
+
+    @staticmethod
+    def _get_minutes_suffix(minutes):
+        if minutes == 1:
+            return f"{minutes} минуту"
+        elif minutes not in (12, 13, 14) and minutes % 10 in (2, 3, 4):
+            return f"{minutes} минуты"
+        else:
+            return f"{minutes} минут"
+
+    @staticmethod
+    def _get_seconds_suffix(seconds):
+        if seconds % 10 == 1 and seconds % 100 != 11:
+            return f"{seconds} секунду"
+        elif seconds % 100 not in (12, 13, 14) and seconds % 10 in (2, 3, 4):
+            return f"{seconds} секунды"
+        return f"{seconds} секунд"
