795d30157dc097e82156a1768f0e11b9ac5919c6
medlcago <sasha.vol40@mail.ru>
2023-08-29 01:49:43 +0300

    Added SummarizeVideo

--- handlers/users/cmd_summary.py
+++ handlers/users/cmd_summary.py
@@ -1,15 +1,15 @@
 import asyncio
 
-from aiogram import Bot
+from aiogram import Bot, flags
 from aiogram import Router
-from aiogram import html
 from aiogram import types
 from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters.command import Command, CommandObject
 
+from data.config import Config
 from decorators import MessageLogging, check_command_args
 from filters import ChatTypeFilter
-from utils.neural_networks import summarize_youtube_video
+from utils.neural_networks import SummarizeVideo
 
 command_summary_router = Router()
 
@@ -17,14 +17,17 @@ command_summary_router = Router()
 @command_summary_router.message(Command(commands=["summary"]), ChatTypeFilter(is_group=False))
 @MessageLogging
 @check_command_args
-async def command_summarize(message: types.Message, command: CommandObject, bot: Bot):
+@flags.rate_limit(rate=60, limit=1, key="summary")
+async def command_summarize(message: types.Message, command: CommandObject, bot: Bot, config: Config):
     url = command.args
     loop = asyncio.get_event_loop()
     sent_message = await message.reply("Обработка запроса, ожидайте")
-    summarize = html.quote(
-        await loop.run_in_executor(None, summarize_youtube_video, url, message.from_user.language_code))
+    summarize_video = SummarizeVideo(api_key=config.openai.api_key, api_base=config.openai.api_base,
+                                     model="gpt-3.5-turbo-16k")
+    response = await loop.run_in_executor(None, summarize_video.summarize_youtube_video, url,
+                                          message.from_user.language_code)
     try:
         await bot.edit_message_text(chat_id=sent_message.chat.id, message_id=sent_message.message_id,
-                                    text=summarize, disable_web_page_preview=True)
+                                    text=response, disable_web_page_preview=True, parse_mode="markdown")
     except TelegramBadRequest as error:
         await bot.edit_message_text(chat_id=sent_message.chat.id, message_id=sent_message.message_id, text=str(error))
--- utils/neural_networks/summarize_video.py
+++ utils/neural_networks/summarize_video.py
@@ -3,53 +3,45 @@ import re
 from typing import Optional
 
 import openai
-from openai import InvalidRequestError
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
 
-from data.config import load_config
 
+class SummarizeVideo:
+    def __init__(self, api_key=None, api_base=None, model=None, languages=("ru", "en", "de", "es")):
+        self.api_key = api_key
+        self.api_base = api_base
+        self.model = model
+        self.languages = languages
 
-def extract_youtube_video_id(url: str) -> Optional[str]:
-    """
-    Links example:
-    1. 'https://www.youtube.com/watch?v=video_id',
-    2. 'https://m.youtube.com/watch?v=video_id',
-    3. 'https://youtu.be/video_id',
-    4. 'https://www.youtube.com/embed/video_id',
-    """
-    match = re.search(r'(?:youtube\.com/(?:watch\?v=|embed/)|youtu\.be/)([a-zA-Z0-9_-]+)', url)
-    if match:
-        video_id = match.group(1)
-        return video_id
+    def get_response(self, text: str, language: str) -> str:
+        if not self.model:
+            raise ValueError("Model is not specified.")
+        try:
+            return self._get_response(text=text, language=language)
+        except Exception as e:
+            logging.error(f'Error processing request: {e}')
+            return "Ошибка обработки запроса. Пожалуйста, повторите попытку."
 
+    def _get_response(self, text: str, language: str) -> str:
+        if not self.api_key:
+            raise ValueError("API Key is not specified.")
+        if not self.api_base:
+            raise ValueError("API Base is not specified.")
 
-def get_video_transcript(video_id: str) -> Optional[str]:
-    try:
-        transcript = YouTubeTranscriptApi.get_transcript(video_id=video_id, languages=["ru", "en", "de", "es"])
-    except TranscriptsDisabled:
-        return
+        openai.api_key = self.api_key
+        openai.api_base = self.api_base
 
-    text = " ".join([line["text"] for line in transcript])
-    return text
+        instructions_map = {
+            "ru": "Пожалуйста, резюмируйте предоставленный текст на русском языке",
+            "en": "Please, summarize the provided text in English"
+        }
 
+        prompt = instructions_map.get(language, instructions_map["en"])
 
-def generate_summary(text: str, language: str = "ru") -> str:
-    config = load_config()
-    openai.api_key = config.openai.api_key
-    openai.api_base = config.openai.api_base
-
-    instructions_map = {
-        "ru": "Пожалуйста, резюмируйте предоставленный текст на русском языке",
-        "en": "Please, summarize the provided text in English"
-    }
-
-    instructions = instructions_map.get(language, instructions_map["en"])
-
-    try:
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k",
+            model=self.model,
             messages=[
-                {"role": "system", "content": instructions},
+                {"role": "system", "content": prompt},
                 {"role": "user", "content": text}
             ],
             temperature=0.2,
@@ -58,24 +50,45 @@ def generate_summary(text: str, language: str = "ru") -> str:
             presence_penalty=0,
             frequency_penalty=0.1,
         )
-
         return response.choices[0].message.content.strip()
-    except InvalidRequestError as e:
-        logging.error(f'Error processing request: {e}')
-        return "Unfortunately, the maximum message length has been exceeded."
 
+    @staticmethod
+    def extract_youtube_video_id(url: str) -> Optional[str]:
+        """
+        Links example:
+        1. 'https://www.youtube.com/watch?v=video_id',
+        2. 'https://m.youtube.com/watch?v=video_id',
+        3. 'https://youtu.be/video_id',
+        4. 'https://www.youtube.com/embed/video_id',
+        """
+        match = re.search(r'(?:youtube\.com/(?:watch\?v=|embed/)|youtu\.be/)([a-zA-Z0-9_-]+)', url)
+        if match:
+            video_id = match.group(1)
+            return video_id
 
-def summarize_youtube_video(video_url: str, language: str = "ru") -> str:
-    video_id = extract_youtube_video_id(video_url)
+    def get_video_transcript(self, video_id: str) -> Optional[str]:
+        try:
+            transcript = YouTubeTranscriptApi.get_transcript(video_id=video_id, languages=self.languages)
+        except TranscriptsDisabled:
+            return
 
-    if video_id is None:
-        return "Ссылка не является ссылкой на видео с YouTube"
+        text = " ".join([line["text"] for line in transcript])
+        return text
 
-    transcript = get_video_transcript(video_id)
+    def generate_summary(self, text: str, language: str) -> str:
+        return self.get_response(text=text, language=language)
 
-    if transcript is None:
-        return f"Для данного видео нет транскрипта на английском или русском языке: {video_url}"
+    def summarize_youtube_video(self, video_url: str, language: str = "ru") -> str:
+        video_id = self.extract_youtube_video_id(url=video_url)
 
-    summary = generate_summary(transcript, language=language)
+        if video_id is None:
+            return "Ссылка не является ссылкой на видео с YouTube"
 
-    return summary
+        transcript = self.get_video_transcript(video_id=video_id)
+
+        if transcript is None:
+            return f"Для данного видео нет транскрипта на английском или русском языке: {video_url}"
+
+        summary = self.generate_summary(text=transcript, language=language)
+
+        return summary
