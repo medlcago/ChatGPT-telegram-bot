75fd8f1748f3e06f24951447fc9deda21c6885bf
medlcago <sasha.vol40@mail.ru>
2023-10-03 16:46:23 +0300

    Temporarily removed GPT-4 models

--- data/config.py
+++ data/config.py
@@ -80,9 +80,6 @@ def load_config(debug: bool = False, path: str | None = None) -> Config:
         ),
         models=ModelsConfig(
             available_models=[
-                "gpt-4",
-                "gpt-4-0314",
-                "gpt-4-0613",
                 "gpt-3.5-turbo",
                 "gpt-3.5-turbo-0301",
                 "gpt-3.5-turbo-0613",
